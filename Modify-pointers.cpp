ed90059d2632df7c4e2f3f9f08f848c6de5eb807
Fareh Iqbal <fareh.legend@gmail.com>
2022-09-21 22:44:00 +0500

    Pointers must know.

---
+++ Modify-pointers.cpp
@@ -0,0 +1,32 @@
+// Not only you can create pointers to a variables you can also edit and modify the pointers.
+
+// Changing the value of pointers also changes the value of original varibale to whom the 
+// pointer was pointing.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+  string *ptr = &food;
+
+  // Output the value of food
+  cout << food << "\n";
+
+  // Output the memory address of food
+  cout << &food << "\n";
+
+  // Access the memory address of food and output its value
+  cout << *ptr << "\n";
+  
+  // Change the value of the pointer
+  *ptr = "Hamburger";
+  
+  // Output the new value of the pointer
+  cout << *ptr << "\n";
+  
+  // Output the new value of the food variable
+  cout << food << "\n";
+  return 0;
+}
