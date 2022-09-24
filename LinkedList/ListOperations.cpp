40e0152e2b81c4844cd2dfe494aa5910a1631c98
Fareh Iqbal <fareh.legend@gmail.com>
2022-09-24 19:17:51 +0500

    Added some basic operations

--- LinkedList/ListOperations.cpp
+++ LinkedList/ListOperations.cpp
@@ -12,7 +12,7 @@ class ListOperations{
     
     // Add new node at front
 
-    void Push (Node** head_ref, int new_data){
+    void insertAtStart (Node** head_ref, int new_data){
 
         // 1. Create a new node
 
@@ -33,5 +33,81 @@ class ListOperations{
 
     }
 
+
+    // Also called append
+    void insertAtEnd(Node** head_ref, int new_data){
+        // 1. Creating a new node
+
+        Node* new_node = new Node();
+
+        // Will use this last pointer to get to the end of list
+        Node* last  = *head_ref;
+
+        // 2. Enter data.
+        new_node -> data = new_data;
+
+        // 3. Since the next of last node is always null
+        new_node -> data = NULL;
+
+        // 4. Locate the current last node
+
+        if(*head_ref == NULL){
+            *head_ref = new_node;
+            return;
+        }
+
+        while ( last -> next != NULL){
+            last = last -> next;
+
+
+        }
+        
+        // 5. Change the last node to the newly added node.
+        last -> next = new_node;
+
+
+
+    }
+
+
+    void insertAfter(Node* prev_node, int new_data){
+
+        // 1. Check
+        if (prev_node == NULL){
+            cout << "The given previous node cannot be null";
+            return;
+
+        }
+        // 2. Create new node
+        Node* new_node = new Node();
+
+        // 3. Add data
+
+        new_node -> data = new_data;
+
+        // 4. Make next of new node as next of prevnode
+        new_node -> next = prev_node -> next;
+
+        // 5. next of prev node as new_node
+
+        prev_node -> next = new_node;
+
+
+    }
+
+
+    bool SearchInList(Node** head_ref, int key){
+
+        Node* curr = *head_ref;
+
+        while (curr != NULL) {
+            if (curr -> data == key) return true;
+                curr = curr -> next;
+
+        }
+
+        return false;
+
+    }
 
 };
